06dd53f7c26c6101c4963650e1768b6fda4f5a85
Lennart Kolmodin <kolmodin@gentoo.org>
2010-12-26 17:53:47 +0000

    Fix error messages with "cabal update" -> "hackport update" Same fix as marty.rosenberg@gmail.com once did for cabal-install-0.8.2, which we now are replacing.

--- cabal-install/Distribution/Client/IndexUtils.hs
+++ cabal-install/Distribution/Client/IndexUtils.hs
@@ -187,7 +187,7 @@ readRepoIndex verbosity repo =
         case repoKind repo of
           Left  remoteRepo -> warn verbosity $
                "The package list for '" ++ remoteRepoName remoteRepo
-            ++ "' does not exist. Run 'cabal update' to download it."
+            ++ "' does not exist. Run 'hackport update' to download it."
           Right _localRepo -> warn verbosity $
                "The package list for the local repo '" ++ repoLocalDir repo
             ++ "' is missing. The repo is invalid."
@@ -203,7 +203,7 @@ readRepoIndex verbosity repo =
         Left  remoteRepo -> warn verbosity $
              "The package list for '" ++ remoteRepoName remoteRepo
           ++ "' is " ++ show (tdDay diff)  ++ " days old.\nRun "
-          ++ "'cabal update' to get the latest list of available packages."
+          ++ "'hackport update' to get the latest list of available packages."
         Right _localRepo -> return ()
 
 -- | It is not necessary to call this, as the cache will be updated when the
@@ -445,7 +445,7 @@ packageIndexFromCache mkPkg hnd = accum mempty []
 
     interror msg = die $ "internal error when reading package index: " ++ msg
                       ++ "The package index or index cache is probably "
-                      ++ "corrupt. Running cabal update might fix it."
+                      ++ "corrupt. Running 'hackport update' might fix it."
 
 ------------------------------------------------------------------------
 -- Index cache data structure
